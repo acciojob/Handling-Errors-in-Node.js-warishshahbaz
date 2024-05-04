cef779192e1e5391f621be642b1071b01c5c70b8
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-05-04 18:02:31 +0530

    impl

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-const fs = require('fs');
+const fs = require("fs");
 
 function printFileContents(filePath) {
   // TODO: Use fs.readFile to read the file contents
+  fs.readFile("./output", () => {
+    console.log("File contents");
+  });
 }
 
+printFileContents();
 // TODO: Call printFileContents with the command-line argument
