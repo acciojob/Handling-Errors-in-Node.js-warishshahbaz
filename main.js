df444a03c5f63bd2c1599331c69b007f5886a5ee
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-05-06 12:40:17 +0530

    impl

--- main.js
+++ main.js
@@ -1,26 +1,27 @@
 const fs = require("fs");
 
-function printFileContents(filePath) {
-  // TODO: Use fs.readFile to read the file contents
+function printFileContents() {
+  // Check if the command-line argument is provided
   if (process.argv.length !== 3) {
     console.error("Usage: node read_file.js <file_path>");
     process.exit(1);
   }
 
+  // Get the file path from the command-line argument
   const filePath = process.argv[2];
 
-  // Read the file asynchronously
+  // Read the file
   fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
       if (err.code === "ENOENT") {
-        console.error("Error: File not found");
+        console.error(`Error: File '${filePath}' not found.`);
       } else {
-        console.error("Error reading file:", err.message);
+        console.error(`Error reading file: ${err.message}`);
       }
-    } else {
-      console.log("File content:");
-      console.log(data);
+      process.exit(1);
     }
+    // Print file contents
+    console.log(data);
   });
 }
 
